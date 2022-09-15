d13dd1be87a8e95eb602df3cf322317370293a18
Ari Makkonen <ari.makkonen@pp3.inet.fi>
2022-09-15 09:49:45 +0300

    Aloitus

---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    internal class Student:Person,IPerson
+    {
+        public void SayName(string value)
+        { Console.WriteLine(value); }   
+    }
+}
